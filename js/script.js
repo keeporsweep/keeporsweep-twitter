6e8c8323b69bdd248326b881be8c22d4bca57c2c
Jan-Christoph Borchardt <hey@jancborchardt.net>
2019-05-12 23:35:27 +0200

    Don't hide Twitter card content
    Signed-off-by: Jan-Christoph Borchardt <hey@jancborchardt.net>

--- js/script.js
+++ js/script.js
@@ -41,7 +41,6 @@ function embedTweet(id_str) {
             document.getElementsByClassName('cards')[0],
             // It seems these parameters are not taken?
             {
-                cards: 'hidden',
                 width: '550',
                 dnt: 'true'
             }
